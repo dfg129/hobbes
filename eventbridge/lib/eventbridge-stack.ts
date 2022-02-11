e3aec6f789fe2f22d00f9621d797acb1e5c4809a
dfg129 <dfg129@gmail.com>
2022-02-11 10:01:02 -0500

    custom event bus

---
+++ eventbridge/lib/eventbridge-stack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+export class EventbridgeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const bus = new events.EventBus(this, 'hobbes-events', {
+      
+    });
+
+    new ssm.StringParameter(this, 'HobbesEventBus', {
+      allowedPattern:'.*',
+      description: 'Custom hobbes event bus name',
+      parameterName: 'HobbesEventBus',
+      tier: ssm.ParameterTier.ADVANCED,
+      stringValue: bus.eventBusName,
+    });
+  }
+}
