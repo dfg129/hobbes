c97f2582f2be847a1f775c60ad7a57521a60a60b
dfg129 <dfg129@gmail.com>
2022-02-14 08:29:50 -0500

    some cleanup

--- dynamodb/lambda_write_to_table/lambda/src/main.rs
+++ dynamodb/lambda_write_to_table/lambda/src/main.rs
@@ -1,17 +1,17 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::model::{AttributeAction, AttributeValue};
+use aws_sdk_dynamodb::{Client, Region};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::{json, Value};
 use tracing::info;
-use aws_sdk_dynamodb::{Client, Region};
-use aws_sdk_dynamodb::model::{AttributeValue, AttributeAction};
-use aws_config::meta::region::RegionProviderChain;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-    .with_max_level(tracing::Level::INFO)
-    .with_ansi(false)
-    .without_time()
-    .init();
+        .with_max_level(tracing::Level::INFO)
+        .with_ansi(false)
+        .without_time()
+        .init();
 
     let func = service_fn(handler);
     lambda_runtime::run(func).await?;
@@ -25,25 +25,26 @@ async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&config);
 
-    add_item(&client, "CreateTableStack-TableOne3E3EE4FC-12RDE7V8W6MVG", "hobbes", "portland").await?;
-    
-    Ok(json!({"message": format!("Hello, {}!", "hobbes") }))
+    add_item(
+        &client,
+        "CreateTableStack-TableOne3E3EE4FC-12RDE7V8W6MVG",
+        "hobbes",
+        "portland",
+    )
+    .await?;
+
+    Ok(json!({ "message": format!("Hello, {}!", "hobbes") }))
 }
- 
-async fn add_item(
-    client: &Client,
-    table: &str,
-    id: &str,
-    city: &str,
-) -> Result<(), Error> {
-    let id_av= AttributeValue::S(id.into());
+
+async fn add_item(client: &Client, table: &str, id: &str, city: &str) -> Result<(), Error> {
+    let id_av = AttributeValue::S(id.into());
     let city_av = AttributeValue::S(city.into());
 
     let request = client
-                .put_item()
-                .table_name(table)
-                .item("id", id_av)
-                .item("city", city_av);
+        .put_item()
+        .table_name(table)
+        .item("id", id_av)
+        .item("city", city_av);
 
     request.send().await?;
     info!("[add_item] :  request sent");
